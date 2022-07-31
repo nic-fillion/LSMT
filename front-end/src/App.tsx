afeb45ed8923a819c524bdf84fcfc2da5cf48cfb
nic-fillion <nic.fillion@hotmail.com>
2022-07-30 22:36:27 -0400

    restructuration

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -3,9 +3,11 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import Dashboard from './components/Dashboard';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Dashboard from './components/Dashboard';
+import Navigation from './components/Navigation';
 
 const mdTheme = createTheme();
 
@@ -14,6 +16,8 @@ function App() {
     <div className="App">
       <ThemeProvider theme={mdTheme}>
         <Box sx={{ display: 'flex' }}>
+          <CssBaseline />
+          <Navigation />
           <Dashboard></Dashboard>
         </Box>
       </ThemeProvider>
